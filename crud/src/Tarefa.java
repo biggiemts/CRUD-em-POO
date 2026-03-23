752ffbcf45518bcae6d88a2b33f6cea5c7ab3b8c
matheus <mathbrayan54@gmail.com>
2026-03-22 23:13:11 -0300

    Add files via upload

---
+++ crud/src/Tarefa.java
@@ -0,0 +1,9 @@
+public class Tarefa {
+    public String nome;
+    public boolean concluida;
+
+    public Tarefa(String tarefaTarefa){
+        this.nome = tarefaTarefa;
+        this.concluida = false;
+    }
+}
